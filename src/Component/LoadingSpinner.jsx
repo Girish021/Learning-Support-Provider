dbefca97ff6e407a9ff64a0c64fdae596a6ab197
Girish021 <girishbathi79@gmail.com>
2025-06-14 04:02:56 +0530

    added spinner for suspense

---
+++ src/Component/LoadingSpinner.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const LoadingSpinner = () => {
+  return (
+    <div>
+      <div className="flex justify-center items-center h-64">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-indigo-500"></div>
+      </div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
