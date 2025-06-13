28d839c88155b346a2ab3c12a5c96d340487d72a
Girish021 <girishbathi79@gmail.com>
2025-06-14 02:32:59 +0530

    Set up routing with Home, Provider detail, and NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Provider from "./pages/ProviderDetail";
+import NotFoundPage from "./pages/NotFoundPage";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/provider/:id" element={<Provider />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
