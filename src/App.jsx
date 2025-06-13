8810a6dc237b91dca2db93adc9644929859e29f3
Girish021 <girishbathi79@gmail.com>
2025-06-14 03:49:51 +0530

    added header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,33 @@
+
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Provider from "./pages/ProviderDetail";
-import NotFoundPage from "./pages/NotFoundPage";
-const App = () => {
+import ProviderDetail from "./pages/ProviderDetail";
+import NotFound from "./pages/NotFound";
+import Header from "./Component/Header";
+
+function App() {
   return (
     <Router>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/provider/:id" element={<Provider />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <div className="min-h-screen flex flex-col bg-gray-50">
+        <Header />
+        <main className="flex-grow container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/provider/:id" element={<ProviderDetail />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <footer className="bg-gray-800 text-white py-4">
+          <div className="container mx-auto px-4 text-center">
+            <p>
+              © {new Date().getFullYear()} Learning Support Directory. All
+              rights reserved.
+            </p>
+          </div>
+        </footer>
+      </div>
     </Router>
   );
-};
+}
 
 export default App;
